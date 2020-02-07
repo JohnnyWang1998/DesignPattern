1e480aae0e868f72c6d993b56dbe56bfcfc862a6
JohnnyWang1998 <48373392+JohnnyWang1998@users.noreply.github.com>
2020-02-07 17:20:49 +0800

    Strategy Pattern-Ducks
    This is the practice of strategy pattern with an example of ducks.

---
+++ src/duck/MallardDuck.java
@@ -0,0 +1,14 @@
+package duck;
+
+public class MallardDuck extends Duck {
+	public MallardDuck() {
+		flyBehavior = new FlyWithWings();
+		quackBehavior = new Quack();
+	}
+	
+	@Override
+	public void display() {
+		System.out.println("MallardDuck here");
+	}
+	
+}
